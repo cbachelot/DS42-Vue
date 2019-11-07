bf6a93342810460c602e1c516376377c36b76b0b
bazartrance <bazarsoundz@gmail.com>
2019-11-07 10:00:11 -0600

    Update navigationcomp.vue

--- src/components/navigationcomp.vue
+++ src/components/navigationcomp.vue
@@ -1,11 +1,9 @@
-
 <template>
 <div class="navigationcomp">
-     
-    <a class="toggle close nav-close-icon" href="#"><img src="../images/close-circle.svg" alt="close icon"/></a>
-    <div>
-    <img class="nav-logo" style="min-width:270px; padding:15px; height:auto;" alt="Design System 42" src="../images/42-ph-logo.svg">
-    </div>
+<a class="toggle close nav-close-icon" href="#"><img src="../images/close-circle.svg" alt="close icon"/></a>
+<div>
+<img class="nav-logo" style="min-width:270px; padding:15px; height:auto;" alt="Design System 42" src="../images/42-ph-logo.svg">
+</div>
 <nav class="nav-effect">
 <div class="ds42-nav-title">Design System Forty Two</div>
 <router-link  to="/">DS 42 Overview</router-link>
